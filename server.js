e5d491dc82c91347adad52645bb52b62a4d3edd1
Amber Shipley <ashipley2@me.com>
2023-08-24 13:35:39 -0700

    Inital commit and create first route

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+// const cors = require("cors");
+const port = 3001;
+app.set("port", port);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`server has started on port ${port} 💸 🔦 💫`);
+});
+
+//routes
+
+app.get("/api/v1/bookshelf", (req, res) => {
+  res.send("GET bookshelf");
+});
+
+// app.post("/", (req, res) => {
+//   res.send("POST request to the homepage");
+// });
